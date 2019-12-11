d4823cf468fc3180c02ef76d2296b74dce788147
Sebastian Szaniszlo <sebastian.szaniszlo@gmail.com>
2019-12-11 21:01:46 +0000

    Set up project and authentication using Feathers

---
+++ src/models/users.model.ts
@@ -0,0 +1,19 @@
+// users-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+import { Application } from '../declarations';
+import { Schema, model } from 'mongoose';
+
+export default function (app: Application) {
+  // const mongooseClient = app.get('mongooseClient');
+
+  const userSchema = new Schema({
+    email: { type: String, unique: true, lowercase: true, required: true },
+    password: { type: String, required: true },
+  }, {
+    timestamps: true,
+  });
+
+  return model('users', userSchema);
+}
